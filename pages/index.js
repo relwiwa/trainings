c8ae8f34d2e94f18e6ff8e756e30b973257466f1
Walter <relw1wa-github@yahoo.de>
2020-01-13 12:13:38 +0100

    Next.js tutorial: API routes section

--- pages/index.js
+++ pages/index.js
@@ -1,73 +1,49 @@
-import Link from "next/link";
+import useSWR from "swr";
+import { useRouter } from "next/router";
 import fetch from "isomorphic-unfetch";
 
-import MyLayout from "../components/MyLayout";
-
-function getPosts() {
-    return [
-        { id: 'hello-nextjs', title: 'Hello Next.js' },
-        { id: 'learn-nextjs', title: 'Learn Next.js is awesome' },
-        { id: 'deploy-nextjs', title: 'Deploy apps with ZEIT' }
-    ];
+function fetcher(url) {
+    return fetch(url).then(r => r.json());
 }
 
-function PostLink({ post }) {
-    return (
-        <li>
-            <Link href={`/post?title=${post.title}`}>
-                <a>{post.title}</a>
-            </Link>
-            <style jsx>{`
-                li {
-                    list-style: none;
-                    margin: 5px 0;
-                }
-                
-                a {
-                    font-family: 'Arial';
-                    text-decoration: none;
-                    color: blue;
-                }
-
-                a:hover {
-                    opacity: 0.6;
-                }
-            `}</style>
-        </li>
+function Index() {
+    const { query } = useRouter();
+    const { data, error } = useSWR(
+        `/api/randomQuote${query.author ? "?author=" + query.author : ""}`,
+        fetcher
     );
-}
 
-function Index({ shows }) {
+    const author = data?.author;
+    let quote = data?.quote;
+
+    if (!data) quote = "Loading...";
+    if (error) quote = "Failed to fetch the quote";
+
     return (
-        <MyLayout>
-            <h1>My Blog</h1>
-            <ul>
-                {getPosts().map(post => (
-                    <PostLink key={post.id} post={post} />
-                ))}
-            </ul>
+        <main className="center">
+            <div className="quote">{quote}</div>
+            {author && <span className="author">- {author}</span>}
             <style jsx>{`
-                h1 {
-                    font-family: 'Arial';
+                main {
+                    width: 90%;
+                    max-width: 900px;
+                    margin: 300px auto;
+                    text-align: center;
                 }
-
-                ul {
-                    padding: 0;
+                .quote {
+                    font-family: cursive;
+                    color: #e243de;
+                    font-size: 24px;
+                    padding-bottom: 10px;
+                }
+                .author {
+                    font-family: sans-serif;
+                    color: #559834;
+                    font-size: 20px;
                 }
             `}</style>
-        </MyLayout>
-    )
+        </main>
+        );
 }
-
-Index.getInitialProps = async function() {
-    const res = await fetch('https://api.tvmaze.com/search/shows?q=batman');
-    const data = await res.json();
-
-    console.log(`Show data fetched. Count: ${data.length}`);
-
-    return {
-        shows: data.map(entry => entry.show)
-    };
-};
 
 export default Index;
