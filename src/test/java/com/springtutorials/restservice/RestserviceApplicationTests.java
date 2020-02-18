fe04de81ea3e322e58fb25f026894a534038da95
Walter <relw1wa-github@yahoo.de>
2020-02-18 15:11:53 +0100

    Initialize tutorial setup via Spring Initializer

---
+++ src/test/java/com/springtutorials/restservice/RestserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springtutorials.restservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
