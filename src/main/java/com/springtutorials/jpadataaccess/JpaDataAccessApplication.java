13eb0de87d702bc3aa0d89dab68a9f14072c7cb4
Walter <relw1wa-github@yahoo.de>
2020-02-18 15:23:04 +0100

    Initialize tutorial setup via Spring Initializer

---
+++ src/main/java/com/springtutorials/jpadataaccess/JpaDataAccessApplication.java
@@ -0,0 +1,13 @@
+package com.springtutorials.jpadataaccess;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaDataAccessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaDataAccessApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springtutorials/jpadataaccess/JpaDataAccessApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springtutorials.jpadataaccess;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaDataAccessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
