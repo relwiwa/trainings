16e8beabf98a79e826124eb0ce858ef46bbc299b
Walter <relw1wa-github@yahoo.de>
2020-02-19 12:49:00 +0100

    Initialize tutorial project via Spring Initializer

---
+++ src/main/java/com/springtutorials/reactandspringrest/ReactAndSpringRestApplication.java
@@ -0,0 +1,13 @@
+package com.springtutorials.reactandspringrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactAndSpringRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactAndSpringRestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springtutorials/reactandspringrest/ReactAndSpringRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springtutorials.reactandspringrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactAndSpringRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
